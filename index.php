34218e05df654f3246dad853519c2b6c827822ef
borilla <github@borilla.co.uk>
2017-09-07 11:58:47 +0800

    Improve html

--- index.php
+++ index.php
@@ -11,16 +11,19 @@ body { font-family: sans-serif; }
 </style>
 </head>
 <body>
+
 <h1>index.php</h1>
+
 <p>
 Path: <strong><?php echo parse_url($_SERVER['REQUEST_URI'])['path'] ?></strong>
 </p>
+
 <p>
 Testing html caching using .htaccess rewrites - compatible with
 <a href="https://www.one.com/">one.com</a> hosting
 </p>
-<p>
-Test links:
+
+<h2>Test links:</h2>
 <ul>
 <li><a href="cached.html">cached.html</a> (will be rewritten to cache/cached.html)</li>
 <li><a href="not-cached.html">not-cached.html</a> (will rewrite to index.php)</li>
@@ -29,13 +32,14 @@ Test links:
 <li><a href="index.php">index.php</a> (access to .php files not allowed)</li>
 <li><a href="image.jpg">image.jpg</a> (is allowed)</li>
 </ul>
-Links with querystrings (won't use cached version):
+
+<h2>Links with querystrings (won't use cached version):</h2>
 <ul>
 <li><a href="cached.html?q=1">cached.html?q=1</a> (will rewrite to index.php)</li>
 <li><a href="not-cached.html?q=1">not-cached.html?q=1</a> (will rewrite to index.php)</li>
 <li><a href="subdir/another.html?q=1">subdir/another.html?q=1</a> (will rewrite to index.php)</li>
 <li><a href="subdir/not-cached.html?q=1">subdir/not-cached.html?q=1</a> (will rewrite to index.php)</li>
 </ul>
-</p>
+
 </body>
 </html>
